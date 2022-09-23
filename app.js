6212b93f75fcbe8ed6aed02f17ff1f5b98bd152c
Imonisa Brian <mcbriandavids43@gmail.com>
2022-09-23 13:23:32 +0100

    First Portfolio Commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const sections = document.querySelectorAll(".section");
+const sectBtns = document.querySelectorAll(".controls");
+const sectBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function pageTransitions() {
+	/*Active-Btn */
+	for (let i = 0; i < sectBtn.length; i++) {
+		sectBtn[i].addEventListener("click", function () {
+			let currentBtn = document.querySelectorAll(".active-btn");
+			currentBtn[0].className = currentBtn[0].className.replace(
+				"active-btn",
+				" "
+			);
+			this.className += " active-btn";
+		});
+	}
+	/**Section Active Class */
+	allSections.addEventListener("click", (e) => {
+		const id = e.target.dataset.id;
+		if (id) {
+			sectBtns.forEach((btn) => {
+				btn.classList.remove("active");
+			});
+			e.target.classList.add("active");
+			/*Hide Other sections */
+			sections.forEach((section) => {
+				section.classList.remove("active");
+			});
+			const element = document.getElementById(id);
+			element.classList.add("active");
+		}
+	});
+	// Toggle Theme
+	const themeBtn = document.querySelector(".theme-btn");
+	themeBtn.addEventListener("click", () => {
+		let element = document.body;
+		element.classList.toggle("light-mode");
+	});
+}
+
+pageTransitions();
